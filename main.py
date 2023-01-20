c55b1a0721c047ef4cba1606a951db7db76ba643
Jacob Hodge <jacob345k@gmail.com>
2023-01-20 14:55:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import subprocess
+import tempfile
 
 folder_path = 'C:\path\to\folder'
 
@@ -20,13 +21,21 @@ def check_ffmpeg_installation():
 check_choco_installation()
 check_ffmpeg_installation()
 
+time_of_check = time.time()
+
+# check for av1 files once upon script startup
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith('.av1'):
+            file_path = os.path.join(root, file)
+            output_path = os.path.splitext(file_path)[0].replace('.av1','') + '.mkv'
+            temp_dir = tempfile.mkdtemp()
+            subprocess.run(['ffmpeg', '-i', file_path, '-c:v', 'libx265', '-c:a', 'copy', '-threads','4', os.path.join(temp_dir,output_path)], shell=True)
+            if os.path.exists(os.path.join(temp_dir,output_path)):
+                os.remove(file_path)
+                os.rename(os.path.join(temp_dir,output_path),file_path)
+                os.rmdir(temp_dir)
+
 while True:
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.av1'):
-                file_path = os.path.join(root, file)
-                output_path = os.path.splitext(file_path)[0] + '.mkv'
-                subprocess.run(['ffmpeg', '-i', file_path, '-c:v', 'libx265', '-c:a', 'copy', '-threads','4', output_path], shell=True)
-                if os.path.exists(output_path):
-                    os.remove(file_path)
-    time.sleep(28800) # 8 hours
+    if time.time() - time_of_check > 172800:  # 2 days
+        for root, dirs, files in os 
